110a0e71096456208d13a72c58d860ec7d90447f
Emilio <pringles212@gmail.com>
2019-05-07 23:25:15 -0400

    Toast
    implemente un toast para no usar alert

--- routes/Api/profile-helper.js
+++ routes/Api/profile-helper.js
@@ -3,7 +3,7 @@ const dbApi = require('../../helpers/database/Api/dbApi');
 
 exports.updateUser = async (req, res) => {
   try {
-    const {name, email, creditCard, creditCard} = req.body;
+    const {name, email, creditCard, bankAccount} = req.body;
     console.log(req.user);
     await dbApi.updateUser(req.user.id, name, email, creditCard, bankAccount);
     res.json({status: 200, message: 'user updated'});
